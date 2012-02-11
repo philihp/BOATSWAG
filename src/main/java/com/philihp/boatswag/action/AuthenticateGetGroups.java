dfa7f876b69337b11fe175bfb6784d731a9f834f
Philihp Busby <philihp@gmail.com>
2012-02-10 23:26:31 -0500

    locked down app so that only aircrew may view it

--- src/main/java/com/philihp/boatswag/action/AuthenticateGetGroups.java
+++ src/main/java/com/philihp/boatswag/action/AuthenticateGetGroups.java
@@ -18,37 +18,46 @@ import org.apache.struts.action.ActionMapping;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.philihp.boatswag.facebook.FBUser;
+import com.philihp.boatswag.facebook.GroupMember;
+import com.philihp.boatswag.facebook.GroupMembers;
+import com.philihp.boatswag.facebook.GroupMembersDeserializer;
 import com.philihp.boatswag.facebook.Groups;
 import com.philihp.boatswag.facebook.GroupsDeserializer;
 
-public class AuthenticateGetGroups extends Action {
+public class AuthenticateGetGroups extends BaseAction {
 
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		Date expires = (Date) request.getSession().getAttribute("accessExpires");
 
-		if (expires == null || expires.before(new Date())) {
+		FBUser credentials = (FBUser) request.getSession().getAttribute("credentials");
+		String accessToken = (String) request.getSession().getAttribute("accessToken");
+		String groupId = (String) getServlet().getServletContext().getAttribute("membership.group.id");
+
+		if (credentials == null)
 			return mapping.findForward("authenticate");
-		} else {
-			String accessToken = (String) request.getSession().getAttribute("accessToken");
-			URL url = new URL("https://graph.facebook.com/me/groups/?access_token=" + accessToken);
-			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-			if (connection.getResponseCode() == 400) {
-				return mapping.findForward("authenticate");
-			} else {
-				InputStream inputStream = connection.getInputStream();
-				Reader reader = new InputStreamReader(inputStream);
 
-				Gson gson = new GsonBuilder().registerTypeAdapter(Groups.class, new GroupsDeserializer())
-						.create();
-				Groups groups = gson.fromJson(reader, Groups.class);
+		URL url = new URL("https://graph.facebook.com/" + groupId + "/members" + "?access_token=" + accessToken);
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		if (conn.getResponseCode() == 400)
+			return mapping.findForward("authenticate");
+		InputStream inputStream = conn.getInputStream();
+		Reader reader = new InputStreamReader(inputStream);
 
-				request.getSession().setAttribute("groups", groups.getGroups());
+		Gson gson = new GsonBuilder().registerTypeAdapter(GroupMembers.class, new GroupMembersDeserializer()).create();
+		GroupMembers groupMembers = gson.fromJson(reader, GroupMembers.class);
 
-				return mapping.findForward("default");
-			}
+		if(groupMembers.getGroupMembers().size() == 0) {
+			return mapping.findForward("not-in-group");
 		}
+		
+		for (GroupMember member : groupMembers.getGroupMembers()) {
+			findConnection(groupId, member.getId());
+			//the act of finding the connection will create it if it doesn't exist.
+			//since we never "clear" the list, people will just be added. but we have a last-updated, kinda... so whatevs bro. good enough!
+		}
+
+		return mapping.findForward("default");
 	}
 
 }
---
+++ src/main/webapp/no-access.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Air Atlas</title>
+<style type="text/css">
+body {
+	font: 81.25%/1.5 Arial, Helvetica, sans-serif;
+}
+</style>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27506789-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+
+<img src="/images/405.jpg" />
+
+<p>You must be a member of the AirFamily group to use this app.</p>
+
+</body>
+</html>
